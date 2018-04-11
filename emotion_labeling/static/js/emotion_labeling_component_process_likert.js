0df383ebbfd77242cf5215a05a723f32aadaaad3
johnr0 <johnr0hol@gmail.com>
2018-04-11 23:31:36 +0900

    unescape

--- emotion_labeling/static/js/emotion_labeling_component_process_likert.js
+++ emotion_labeling/static/js/emotion_labeling_component_process_likert.js
@@ -24,7 +24,7 @@ for(var key in prompt_time){
 }
 times.sort(function(a, b){return a-b;})
 if(condition.includes("experiment")){
-  video_url = primitive_video_url.replace(/&#39;/g, "'");//media/uniform/"+primitive_video_url+".mp4?raw=true"
+  video_url = unescape(primitive_video_url);//media/uniform/"+primitive_video_url+".mp4?raw=true"
 }
 
 // Below part is for vue app
