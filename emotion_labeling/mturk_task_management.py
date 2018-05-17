c13282797ba2fa4a43014155e88816a807fffbb2
johnr0 <johnr0hol@gmail.com>
2018-05-17 20:52:49 +0900

    key required

--- emotion_labeling/mturk_task_management.py
+++ emotion_labeling/mturk_task_management.py
@@ -1,5 +1,6 @@
 import boto3
 from .models import *
+from .MTURKKEY import *
 
 MTURK_SANDBOX = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
 TIME_FOR_EACH_PROMPT = 180
@@ -150,7 +151,7 @@ if (mturkworkerID == "" && assignmentID == "" ) {
 """
 
 
-mturk = boto3.client('mturk', aws_access_key_id = '', aws_secret_access_key = '', region_name='us-east-1', endpoint_url=MTURK_SANDBOX)
+mturk = boto3.client('mturk', aws_access_key_id = ACCESS_KEY, aws_secret_access_key = SECRET_KEY, region_name='us-east-1', endpoint_url=MTURK_SANDBOX)
 
 
 def Create_Emotion_Distribution_collection_HIT(title):
