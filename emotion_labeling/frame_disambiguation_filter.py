112fe76b067bccc0f6565c32a42e9c9f88690de1
johnr0 <johnr0hol@gmail.com>
2018-06-19 18:29:44 +0900

    a worker non replicated tasks

--- emotion_labeling/frame_disambiguation_filter.py
+++ emotion_labeling/frame_disambiguation_filter.py
@@ -58,7 +58,7 @@ def initialize_frame_sentences():
         if index not in exclude_list:
             fs = Frame_Sentence(sentence_id = index)
             fs.save()
-            #if Frame_Sentence.objects.all().count() == 16:
+            #if Frame_Sentence.objects.all().count() == 11:
             #    break
 
 #to generate frame tasks when a worker comes in initially
@@ -68,8 +68,11 @@ def frame_initialize_worker(wid, aid, frame_task):
     ft.save()
     for i in range(1, TOTAL_SUB_TASK_NUM):
         excludable = frame_task.objects.filter(wid=wid)
-        #taskable_sentence = Frame_Sentence.objects.exclude(frame_task__in = excludable)
-        taskable_sentence = Frame_Sentence.objects.annotate(num_task=Count(frame_task.model_name())).filter(num_task__lt=TARGET_TASK_NUM)
+        kwargs = {
+            frame_task.model_name()+"__in" : excludable,
+        }
+        taskable_sentence = Frame_Sentence.objects.exclude(**kwargs)
+        taskable_sentence = taskable_sentence.annotate(num_task=Count(frame_task.model_name())).filter(num_task__lt=TARGET_TASK_NUM)
         min_ = taskable_sentence.aggregate(min_num_task=Min('num_task'))
         min_num_task = min_['min_num_task']
         print(min_)
